b9a2b06fbbbc7c9df214d14e43072d0f4c209adc
Jia Jia <angersax@sina.com>
2019-12-12 14:55:17 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#sudo apt install upx
+go env -w GOPROXY=https://goproxy.cn,direct
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/mailsender src/main.go
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -ldflags="-s -w" -o bin/mailsender.exe src/main.go
 
-export GOPROXY=https://goproxy.io,direct
-GOOS=linux go build -ldflags="-s -w" -o bin/mailsender src/main.go
-GOOS=windows go build -ldflags="-s -w" -o bin/mailsender.exe src/main.go
+#apt install upx
 upx bin/mailsender
+upx bin/mailsender.exe
