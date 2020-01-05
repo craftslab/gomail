c288e70bbb1bf77fa074d92f61d9359ff57bfe90
Jia Jia <angersax@sina.com>
2020-01-05 11:37:17 +0800

    Set version to v2.0.0

--- src/sender.go
+++ src/sender.go
@@ -27,7 +27,7 @@ import (
 
 const (
     author  = "Jia Jia"
-    version = "1.2.0"
+    version = "2.0.0"
 )
 
 type Config struct {
--- tools/recipientparser/parser.go
+++ tools/recipientparser/parser.go
@@ -29,7 +29,7 @@ import (
 
 const (
     author  = "Jia Jia"
-    version = "1.2.0"
+    version = "2.0.0"
 )
 
 type Config struct {
