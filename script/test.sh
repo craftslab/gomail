dc1898985d0b47474ec0ea8f955995502cb14849
Jia <49358172+craftslab@users.noreply.github.com>
2022-02-07 15:01:09 +0800

    Update test.sh

--- script/test.sh
+++ script/test.sh
@@ -3,12 +3,12 @@
 go env -w GOPROXY=https://goproxy.cn,direct
 
 if [ "$1" = "all" ]; then
-  GIN_MODE=release go test -cover -v -parallel 2 ./...
+  go test -cover -v -parallel 2 ./...
 else
   list="$(go list ./... | grep -v foo)"
   old=$IFS IFS=$'\n'
   for item in $list; do
-    GIN_MODE=release go test -cover -v -parallel 2 "$item"
+    go test -cover -v -parallel 2 "$item"
   done
   IFS=$old
 fi
