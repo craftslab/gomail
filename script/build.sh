4eb920370d930270f5712c80640dc617619fb2d2
Jia <49358172+craftslab@users.noreply.github.com>
2022-02-07 15:00:53 +0800

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -7,8 +7,8 @@ list="parser,sender"
 old=$IFS IFS=$','
 
 for item in $list; do
-  GIN_MODE=release CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "$ldflags" -o bin/$item $item/$item.go
-  GIN_MODE=release CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -ldflags "$ldflags" -o bin/$item.exe $item/$item.go
+  CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "$ldflags" -o bin/$item $item/$item.go
+  CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -ldflags "$ldflags" -o bin/$item.exe $item/$item.go
   upx bin/$item
   upx bin/$item.exe
 done
