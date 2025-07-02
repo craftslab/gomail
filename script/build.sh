8e064c5c19b003dd4208befc9fb82e28ccfff0e4
Jia Jia <angersax@sina.com>
2025-07-02 10:56:09 +0800

    feat(script): add support to compress executable file

--- script/build.sh
+++ script/build.sh
@@ -8,3 +8,9 @@ target="sender"
 go env -w GOPROXY=https://goproxy.cn,direct
 
 CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o bin/$target sender/sender.go
+
+if ! command -v upx &> /dev/null; then
+  sudo apt-get update
+  sudo apt-get install -y upx-ucl
+fi
+upx bin/$target
